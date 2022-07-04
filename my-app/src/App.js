7accb95890db6d56aae95bd2a80bc15a4df9e288
Khalmatay <khalmatay@gmail.com>
2022-07-04 16:58:38 +0600

    dobavil auth

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./App.css"
+import { Routes, Route , MemoryRouter} from "react-router-dom"
+import Auth from "./Auth"
+import Sign from "./signup-succes"
 function App() {
-  console.log('HELLO');
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    
+    <MemoryRouter>
+      <Routes>
+        <Route path="/" element={<Auth />} />
+        <Route path="/nur" element={<Sign/>} />
+      </Routes>
+    </MemoryRouter>
+  )
 }
 
 export default App;
